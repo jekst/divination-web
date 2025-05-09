edc9167799d0eef0aa761b124c786944333cbffc
jekst <jekst@qq.com>
2025-05-09 09:42:21 +0800

    change icon

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 import { ref } from 'vue'
-import { isDate } from 'element-plus/es/utils/types.mjs'
 const activeIndex = ref('1')
 const media = window.matchMedia('(prefers-color-scheme: dark)')
 function changeColorMode(isDark: boolean) {
@@ -24,12 +23,11 @@ if (media) {
 			class="menu"
 			mode="horizontal"
 			router="true"
-			:ellipsis="false">
-			<el-menu-item index="0" route="/">
-				<img
-					style="width: 30px"
-					src="./assets/logo.png"
-					alt="吉庆六爻" />
+			:ellipsis="false"
+		>
+			<el-menu-item index="0" route="/" style="padding: 0">
+				<img style="width: 30px" src="./assets/logo.png" />
+				易盘
 			</el-menu-item>
 			<el-menu-item index="1" route="/">排盘 </el-menu-item>
 			<el-menu-item index="2" route="/liuyao/history">记录 </el-menu-item>
